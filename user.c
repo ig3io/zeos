d548dd335a4fc43c6b22dc6701fc1e5268836373
Ignacio Contreras <ignacio@nachovia.com>
2013-02-19 20:00:07 +0100

    Testing write function from user code. DOESN'T WORK

--- user.c
+++ user.c
@@ -10,6 +10,10 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    
+  char test[3];
+  test[0] = 'A';
+  test[1] = 'B';
+  test[2] = '\0';
+  write(1, test, 3);   
   while(1) { }
 }
