1f9b8fc46f76c28015a763be629462fec5701a96
Ignacio Contreras <ignacio@nachovia.com>
2013-02-19 17:15:37 +0100

    system does not initializes zeos_ticks, maybe that should change in the future

--- system.c
+++ system.c
@@ -61,9 +61,6 @@ inline void set_seg_regs(Word data_sel, Word stack_sel, DWord esp)
 int __attribute__((__section__(".text.main"))) 
   main(void) 
 {
-  /* Global variables initialization */
-  zeos_ticks = 0;
-
   set_eflags();
 
   /* Define the kernel segment registers */
